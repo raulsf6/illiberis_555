ebc82e6e0ebeacd3d1d437cab3f7203c4a6273a8
Raul Sanchez <raulsf.it@gmail.com>
2023-03-10 18:12:16 +0100

    Added header component

---
+++ src/components/header_middle.js
@@ -0,0 +1,48 @@
+const navigation = [
+  { name: 'Quienes somos', href: '#' },
+  { name: 'Secciones', href: '#' },
+  { name: 'Actividades', href: '#' },
+]
+
+export default function Header() {
+  return (
+    <header className="bg-indigo-600">
+      <nav className=" mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+        <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+          <div className="flex items-center">
+            <a href="#">
+              <span className="sr-only">Workflow</span>
+              <img
+                className="h-10 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
+                alt=""
+              />
+            </a>
+            <div className="hidden ml-10 space-x-8 lg:block">
+              {navigation.map((link) => (
+                <a key={link.name} href={link.href} className="text-base font-medium text-white hover:text-indigo-50">
+                  {link.name}
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="ml-10 space-x-4" >
+            <button
+              onClick={tusMuertos}
+              className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
+            >
+              Login
+            </button>
+          </div>
+        </div>
+        <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+          {navigation.map((link) => (
+            <a key={link.name} href={link.href} className="text-base font-medium text-white hover:text-indigo-50">
+              {link.name}
+            </a>
+          ))}
+        </div>
+      </nav>
+    </header>
+  )
+}
