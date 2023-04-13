606f972c926067f57716e4cefe032ee9ee3d565a
Raul Sanchez <raulsf.it@gmail.com>
2023-04-13 19:38:44 +0200

    included person in aboutus

---
+++ src/data/members.js
@@ -0,0 +1,9 @@
+const members = [
+    {
+        name: "Jorge Sánchez",
+        position: "Coordinador",
+        image: "default_user.png"
+    }
+];
+
+export default members;
--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -1,4 +1,6 @@
 import Quote from "@/components/quote";
+import Person from "@/components/people_card";
+import members from "@/data/members";
 
 var quote = "Las creencias son actos de fe, la vida se hace con hechos"
 var author = "Isaac Bhenaggay"
@@ -6,6 +8,16 @@ var position = "Coordinador"
 
 export default function AboutUs() {
     return (
+        <>
         <Quote quote={quote} author={author} position={position} />
+        <div class="text-center">
+            <p class="text-3xl font-bold my-1 lg:my-5">El equipo</p>
+            <ul role="list" class="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6">
+                {members.map(
+                    member => <li><Person name={member.name} position={member.position} image={member.image}/></li>
+                )}   
+            </ul>
+        </div>
+        </>
     )
   }
