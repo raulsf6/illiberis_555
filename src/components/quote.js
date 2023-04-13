e5e6436ac3357b82fac18a2264c00c88b2e55e28
Raul Sanchez <raulsf.it@gmail.com>
2023-04-13 19:38:12 +0200

    removed image from quote

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,9 +1,9 @@
 export default function Quote(props) {
     return (
-      <section className="py-12 bg-gray-50 overflow-hidden md:py-20 lg:py-24">
+      <section className="py-1 bg-gray-50 overflow-hidden md:py-6 mb-10 lg:py-10">
         <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="relative">
-            <blockquote className="mt-10">
+            <blockquote className="mt-3">
               <div className="max-w-3xl mx-auto text-center text-2xl leading-9 font-medium text-gray-900">
                 <p>
                   &ldquo;{props.quote}&rdquo;
@@ -11,13 +11,6 @@ export default function Quote(props) {
               </div>
               <footer className="mt-8">
                 <div className="md:flex md:items-center md:justify-center">
-                  <div className="md:flex-shrink-0">
-                    <img
-                      className="mx-auto h-11 w-11 rounded"
-                      src=""
-                      alt=""
-                    />
-                  </div>
                   <div className="mt-3 text-center md:mt-0 md:ml-4 md:flex md:items-center">
                     <div className="text-base font-medium text-gray-900">{props.author}</div>
   
