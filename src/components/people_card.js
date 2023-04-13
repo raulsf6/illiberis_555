c8125b2999dda750eaccf0b0c35be0c564080771
Raul Sanchez <raulsf.it@gmail.com>
2023-04-13 19:38:29 +0200

    added Person component

---
+++ src/components/people_card.js
@@ -0,0 +1,18 @@
+export default function Person(props) {
+    console.log("image value is: " + props.image)
+  return (
+    <div class="space-y-4">
+      <img
+        class="mx-auto h-20 w-20 rounded-full lg:w-24 lg:h-24"
+        src={props.image == undefined ? "default_user.png" : props.image}
+        alt=""
+      />
+      <div class="space-y-2">
+        <div class="text-xs font-medium lg:text-sm">
+          <h3>{props.name}</h3>
+          <p class="text-indigo-600">{props.position}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
