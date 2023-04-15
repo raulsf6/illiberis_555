e6f5d23a231b415d2773f2e19d4d986082495fd1
Raul Sanchez <raulsf.it@gmail.com>
2023-04-15 21:53:07 +0200

    added sections data

---
+++ src/data/group_sections.js
@@ -0,0 +1,7 @@
+export const sections = [
+  {
+    name: "Clan Rover Viejo Chacal",
+    description:
+      "Jóvenes entre 12 y 22 años que buscan seguir enriqueciendo su experiencia scout y desarrollar habilidades de liderazgo y servicio a la comunidad. Los miembros del clan rover trabajan juntos en proyectos comunitarios y organizan actividades de aventura y liderazgo para otros miembros del grupo scout.",
+  },
+];
