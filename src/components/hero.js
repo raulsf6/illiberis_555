b660f49e434dc3e4bd5289c07521c35c03f53525
Raul Sanchez <raulsf.it@gmail.com>
2023-04-15 21:52:52 +0200

    refacored hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
       <div class="mx-auto max-w-7xl w-full pt-16 pb-20 my-10 text-center lg:py-48 lg:text-left lg:items-center">
         <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
           <h1 class="text-4xl tracking-tight font-extrabold text-blue-800 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-            <span class="block inline">Grupo Scout <span class="block text-yellow-300 inline">555</span></span>
+            <span class="block">Grupo Scout <span class="block text-yellow-300">555</span></span>
             <span class="block">Illíberis</span>
           </h1>
           <p class="mt-3 max-w-md mx-auto text-lg sm:text-xl md:mt-5 md:max-w-3xl">Grupo scout ubicado en Ogíjares, Granada</p>
