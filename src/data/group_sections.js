c5243ed071f25571724fc184bac7d08cd22df6c0
Raul Sanchez <raulsf.it@gmail.com>
2023-04-18 21:09:43 +0200

    added sections data

--- src/data/group_sections.js
+++ src/data/group_sections.js
@@ -1,7 +1,22 @@
 export const sections = [
   {
-    name: "Clan Rover Viejo Chacal",
-    description:
-      "Jóvenes entre 12 y 22 años que buscan seguir enriqueciendo su experiencia scout y desarrollar habilidades de liderazgo y servicio a la comunidad. Los miembros del clan rover trabajan juntos en proyectos comunitarios y organizan actividades de aventura y liderazgo para otros miembros del grupo scout.",
+    name: "Manada",
+    description: "La Manada es la sección scout para niños y niñas entre 7 y 11 años, donde se promueve la educación en valores y el trabajo en equipo a través de juegos y actividades lúdicas.",
+    color: ""
   },
+  {
+    name: "Tropa Scout Impeesa",
+    description: "La Tropa es la sección para jóvenes entre 12 y 15 años, donde se fomenta la autonomía y el liderazgo a través de retos y aventuras en la naturaleza, desarrollando habilidades y valores que les permitan ser ciudadanos responsables.",
+    color: ""
+  },
+  {
+    name: "Unidad Esculta Brownsea",
+    description: "La Unidad es la sección para jóvenes entre 16 y 18 años, donde se prepara a los scouts para ser líderes y responsables en su comunidad y en el mundo, desarrollando proyectos y acciones de impacto social.",
+    color: ""
+  },
+  {
+    name: "Clan Rover Viejo Chacal",
+    description: "El Clan es la sección para jóvenes mayores de 18 años, donde se busca profundizar en el compromiso social y en la formación de líderes capaces de enfrentar desafíos complejos, a través de proyectos y actividades que buscan mejorar la calidad de vida de la sociedad.",
+    color: ""
+  }
 ];
